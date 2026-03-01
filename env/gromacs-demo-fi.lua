a02f93dfc69df4e2487dbb468f1d8d9a44ac55e4
Rasmus Kronberg <rasmus.kronberg@csc.fi>
2026-03-01 18:08:32 +0200

    update

---
+++ env/gromacs-demo-fi.lua
@@ -0,0 +1,9 @@
+-- Jupyter
+
+prepend_path('PATH',"/appl/soft/chem/tykky/gromacs-demo/bin")
+
+setenv("_COURSE_BASE_NAME","gromacs-demo-fi")
+setenv("_COURSE_NOTEBOOK","gromacs-demo/gromacs-fi.ipynb")
+setenv("_COURSE_GIT_REPO","https://github.com/csc-training/gromacs-demo")
+setenv("_COURSE_GIT_REF","")
+setenv("_COURSE_NOTEBOOK_TYPE","lab")
